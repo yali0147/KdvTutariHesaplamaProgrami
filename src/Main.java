7426977b40c26b91697736ec00660168688a98a2
yali <yusufali0147@hotmail.com>
2023-08-10 00:43:56 +0300

    modifed src or add jpg

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double tutar, kdvsiz, kdvTutari,kdvOrani ;
+        double kdvli, tutar, kdvTutari,kdvOrani ;
 
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Lütfen Tutarı Giriniz : ");
-        tutar = inp.nextDouble();
-        if (tutar > 0 && tutar <= 1000) {
+        try (Scanner inp = new Scanner(System.in)) {
+            System.out.print("Lütfen Tutarı Giriniz : ");
+            tutar = inp.nextDouble();
+
+        }
+        if ((tutar > 0) && (tutar <= 1000))
             kdvOrani = 0.18;
-        } else if (tutar > 1000) {
-            kdvOrani =0.08;
-        } else {
+        else if (tutar > 1000)
+            kdvOrani = 0.08;
+        else {
             System.out.println("Hatalı giriş! Lütfen pozitif bir sayı giriniz.");
             return;
         }
+
         kdvTutari = (tutar*kdvOrani);
-        kdvsiz =tutar-kdvTutari;
-        System.out.println("KDV'siz Fiyat : "+kdvsiz);
-        System.out.println("KDV Oranı:" +kdvOrani);
-        System.out.println("KDV'li Fiyat : "+ tutar);
+        kdvli =tutar+kdvTutari;
+
+        System.out.println("KDV'siz Tutar : "+tutar);
+        System.out.println("KDV Oranı : " +kdvOrani);
+        System.out.println("KDV'li Tutar : "+ kdvli);
         System.out.println("KDV Tutarı : "+kdvTutari);
 
     }
